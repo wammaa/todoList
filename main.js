1e364e27f5728c8175e718ad593a41d3cac0a801
Hwiho Lee <lhh323@gmail.com>
2024-02-15 00:44:52 +0900

    2/14

--- main.js
+++ main.js
@@ -1,29 +1,66 @@
-let taskInput = document.getElementById('task-input');
-let addButton = document.getElementById('add-button');
+let taskInput = document.getElementById("task-input");
+let addButton = document.getElementById("add-button");
 let taskList = [];
 
-addButton.addEventListener('click',addTask)
+addButton.addEventListener("click",addTask)
 
 function addTask(){
-  let taskContent = taskInput.value
-  taskList.push(taskContent)
+  let task = {
+    id:randomIDGenerate(),
+    taskContent : taskInput.value,
+    isComplete : false
+  }
+  taskList.push(task)
   console.log(taskList)
   render()
 }
 
 function render(){
-  let resultHTML = '';
+  let resultHTML = "";
   for(let i = 0; i<taskList.length; i++){
-    resultHTML +=`<div class="task">
-    <div>${taskList[i]}</div>
+    if(taskList[i].isComplete == true){
+      resultHTML+=`<div class="task">
+      <div class="task-done">${taskList[i].taskContent}</div>
+      <div>
+        <button onclick="toggleComplete('${taskList[i].id}')" class="return-button"><i class="fa-solid fa-arrow-rotate-left"></i></button>
+        <button onclick="deleteTask('${taskList[i].id}')" class="delete-button"><i class="fa-solid fa-trash"></i></button>
+      </div>
+    </div>`
+    }else{ resultHTML +=`<div class="task">
+    <div>${taskList[i].taskContent}</div>
     <div>
-      <button>Check</button>
-      <button>Delete</button>
+      <button onclick="toggleComplete('${taskList[i].id}')" class="check-button"><i class="fa-solid fa-check"></i></button>
+      <button onclick="deleteTask('${taskList[i].id}')" class="delete-button"><i class="fa-solid fa-trash"></i></button>
     </div>
   </div>`;
+    }
   }
-  document.getElementById('task-board').innerHTML = resultHTML;
+  document.getElementById("task-board").innerHTML = resultHTML;
 }
 
-document.getElementById("test").innerHTML = '<h1>noona</h1>'
-document.getElementById("test").textContent = '<h1>noona</h1>'
+
+function toggleComplete(id){
+  for(let i = 0; i < taskList.length; i++){
+    if(taskList[i].id == id){
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  render();
+  console.log(taskList);
+}
+
+function deleteTask(id){
+  for(let i = 0; i < taskList.length; i++){
+    if(taskList[i].id == id){
+      taskList.splice(i,1)
+      break;
+    }
+  }
+  render();
+}
+
+function randomIDGenerate(){
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
+
